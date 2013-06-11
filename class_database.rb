ba3d801ef98ebb2583ab39bcda74c4fcede1cbbb
Inframatic <isteffy@hotmail.com>
2013-06-11 16:00:08 -0400

    Commit 2. attribute search

--- class_database.rb
+++ class_database.rb
@@ -13,11 +13,17 @@ class Database
 	end	
 
 	def display_all_contacts
-
 		@databasearray.each do |contact|
-			puts "id: #{contact.id}\n" + "firstname: #{contact.firstname}\n" + "lastname: #{contact.lastname}\n" + "email: #{contact.email}\n" + "notes: #{contact.notes}\n"
+			return "id: #{contact.id}\n" + "firstname: #{contact.firstname}\n" + "lastname: #{contact.lastname}\n" + "email: #{contact.email}\n" + "notes: #{contact.notes}\n"
 		end
-
 	end
 
+	def search_by_name(name)
+		@databasearray.each do |contact|
+			if name == contact.firstname
+			 return contact
+			end
+		end
+	end
+		
 end
--- testtdd/test_database.rb
+++ testtdd/test_database.rb
@@ -8,7 +8,8 @@ class TestDatabase <Test::Unit::TestCase
 		@contact = Contact.new(id: 0, firstname: "Will",lastname: "Richman", email: "will@bit.com", notes: "cool guy")
 		@contact2 = Contact.new(id: 1, firstname: "Ian",lastname: "Steffy", email: "isteffy@hotmail.com", notes: "coolest guy")
 		@db = Database.new
-
+		@db.add(@contact)
+		@db.add(@contact2)
 	end
 
 	def test_initialize
@@ -16,20 +17,23 @@ class TestDatabase <Test::Unit::TestCase
 	end
 
 	def test_add
-		@db.add(@contact)
 		assert @db.databasearray.include? @contact
 		assert_equal "Will",@db.databasearray[0].firstname
 	end
 
 	def test_display_all_contacts
-		@db.add(@contact)
-		@db.add(@contact2)
 		@db.display_all_contacts
 			assert_equal "Will",@db.databasearray[0].firstname
 			assert_equal "Ian",@db.databasearray[1].firstname
 		assert @db.databasearray.include? @contact && @contact2	
-		
-
 	end
+
+	def test_search_by_name
+		@db.search_by_name(@name)
+		assert_equal @contact, @db.search_by_name("Will")
+	end
+
+	def test_inf
+
 
 end
